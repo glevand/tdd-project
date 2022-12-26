d534c678cfff597fc8fbc2b8f06ed0af22731ca3
Geoff Levand <geoff@infradead.org>
2022-12-25 17:19:52 -0800

    jenkins: Update kernel jobs
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- jenkins/jobs/kernel/linux-6.1.y-stable-trigger.groovy
+++ jenkins/jobs/kernel/linux-6.1.y-stable-trigger.groovy
@@ -7,6 +7,6 @@ script {
 
 kernelTrigger {
     git_url = 'https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git'
-    git_branch = 'linux-4.20.y'
+    git_branch = 'linux-6.1.y'
     cron_spec = 'H H/6 * * *' // Every 6 Hrs.
 }
